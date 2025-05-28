4d976d5a329ea02f1675c17409dc09f03e95408b
SquareExStudios <squareexstudios@gmail.com>
2025-05-28 13:05:03 +0530

    update nav js

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,20 @@
-		// Switch navbar logo based on background color (scroll)
-		$(document).ready(function () {
-			function updateLogo() {
-				var navbar = $('.ftco_navbar');
-				var logo = $('#navlogo');
-				if (navbar.hasClass('scrolled')) {
-					logo.attr('src', '/logo/logof.png');
-				} else {
-					logo.attr('src', '/logo/logo-w.png');
-				}
-			}
+// Switch navbar logo based on background color (scroll)
+$(document).ready(function () {
+  function updateLogo() {
+    var navbar = $(".ftco_navbar");
+    var logo = $("#navlogo");
+    if (navbar.hasClass("scrolled")) {
+      logo.attr("src", "/logo/logof.png");
+    } else {
+      logo.attr("src", "/logo/logo-w.png");
+    }
+  }
 
-			$(window).on('scroll', function () {
-				updateLogo();
-			});
+  $(window).on("scroll", function () {
+    updateLogo();
+  });
 
-			// Initial check
-			updateLogo();
-		});
+  // Initial check
+  updateLogo();
+});
+
